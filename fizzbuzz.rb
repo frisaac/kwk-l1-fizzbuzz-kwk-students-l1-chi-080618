76b0ee96b60958320afad6ec996a7abd8781ef5d
frisaac <frisaac@stu.naperville203.org>
2018-08-07 19:13:48 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(number)
   if number%3 == 0 || number%5 == 0
     if number%3 == 0 
-       "Fizz"
+       print "Fizz"
     end
     if number%5 == 0 
-      "Buzz"
+      print "Buzz"
     end
   else
     nil
