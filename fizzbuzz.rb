73ef062f58ace18df7f0c8af9b3220c982166dfe
frisaac <frisaac@stu.naperville203.org>
2018-08-07 19:06:18 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,19 @@
 def fizzbuzz(number)
+  if(number%3 = 0)
+    print "Fizz"
+  end
+  if(number%5 = 0)
+    print "Buzz"
+  end
 #  if number % 15 == 0
  #    "FizzBuzz"
-  elsif number % 5 == 0
-     "Buzz"
-  elsif number % 3 == 0
-     "Fizz"
-  else
-     nil
-  end
-end
+#  elsif number % 5 == 0
+ #    "Buzz"
+ # elsif number % 3 == 0
+#     "Fizz"
+#  else
+#     nil
+ # end
+#end
 
 fizzbuzz(1)
